1c7d61139a03e0758d44b41744fa8280787e85af
Chetan Luthra <chetanluthra9998@gmail.com>
2020-06-28 01:34:40 +0530

    read from firebase

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import { startSetExpenses } from './actions/expenses'
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -14,5 +15,8 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
+ReactDOM.render(<p>Loading....</p>, document.getElementById('app'));
 
-ReactDOM.render(jsx, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(()=> {
+  ReactDOM.render(jsx, document.getElementById('app'));
+})
